72caa7bc456dfb99ee2fec7aec60c9b369aa71ca
Benjamin Cook <benjamincook1313@gmail.com>
2022-12-07 13:21:39 -0700

    finished functions 2 demo

--- functions-2-demo/starter.js
+++ functions-2-demo/starter.js
@@ -4,6 +4,29 @@
 
 // CODE HERE
 
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2; 
+
+const multiply = (num1, num2) => num1 * num2; 
+
+const divide = (num1, num2) => num1 / num2; 
+
+const calculator = (num1, num2, callback) => {
+  if(+num1 && +num2){
+    num1 = +num1
+    num2 = +num2
+    return callback(num1, num2)
+  }else{
+    console.log("you need to send in numbers");
+  }
+}
+
+const result = calculator(1, 2, add);
+const res2 = calculator(3, "four", multiply);
+
+console.log(result, res2);
+
 ///////////////////////
 ////// PET STORE //////
 ///////////////////////
@@ -64,11 +87,75 @@ const catProducts = [
 
 // CODE HERE
 
+const applyPercentDiscount = (product, discount) => {
+  product.displayPrice = Math.round(product.basePrice * (1 - discount) * 100) / 100;
+}
 
+const applyFlatRateDiscount = (product, discount) => {
+  product.displayPrice = product.basePrice - discount;
+}
 
+const applyDiscount = (arr, callback, discount) => {
+  for(let i=0; i<arr.length; i++){
+    callback(arr[i], discount)
+  }
+} 
+
+// applyDiscount(dogProducts, applyPercentDiscount, .2)
+// console.log(dogProducts);
+
+// applyDiscount(catProducts, applyFlatRateDiscount, 2)
+// console.log(catProducts);
+
+const applyDiscountByCategory = (arr, category, callback, discount) => {
+  for(let i=0; i<arr.length; i++){
+    if(arr[i].category === category){
+      callback(arr[i], discount)
+    }
+  }
+}
+
+// applyDiscountByCategory(catProducts, 2, applyPercentDiscount, .10)
+// console.log(catProducts);
+
+// applyDiscountByCategory(dogProducts, 1, applyFlatRateDiscount, 3)
+// console.log(dogProducts);
+
+const applyDiscountByInventory = (arr, amount, callback,  discount) => {
+  for(let i=0; i<arr.length; i++){
+    if(arr[i].inventory > amount){
+      callback(arr[i], discount)
+    }
+  }
+}
+
+applyDiscountByInventory(catProducts, 55, applyPercentDiscount, .10)
+console.log(catProducts);
+
+applyDiscountByInventory(dogProducts, 55, applyFlatRateDiscount, 3)
+console.log(dogProducts);
 
 ////////////////////////
 ////// SANDWICHES //////
 ////////////////////////
 
 // CODE HERE
+
+function makeSandwich(bread){
+  
+  return (ingredients) => {
+    let order = `You ordered a  ${bread} bread sandwich with `;
+
+    for(let i=0; i<ingredients.length; i++){
+      (i === ingredients.length - 1 && i !== 0)? order += `and ${ingredients[i]}.`
+        : order += `${ingredients[i]}, `
+    }
+    return order;
+  }
+}
+
+const makeWheatSandwich = makeSandwich("wheat");
+const makeRyeSandwich = makeSandwich("rye");
+
+console.log(makeWheatSandwich(["pickles", "cheese", "ham", "lettuce"]));
+console.log(makeRyeSandwich(["turkey"]));
